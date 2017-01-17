df928490ba49a59e2b474738cf13502252a0d891
SamuelioX <samueliox@gmail.com>
2017-01-17 01:30:02 -0800

    refactored functions to print out to a div

--- js/LatLong.js
+++ js/LatLong.js
@@ -32,46 +32,50 @@ $(function () {
                     "$limit": 10000
 ////                "$where": "longitude=-122.282103194"
 //                    "$where": "within_circle(location, " + latit + ", " + longit + ", " +
-//                            1000 + ")"
+//                            radius + ")"
                 }
             }).done(function (data) {
-//            for(var i = 0; i < data.length; i++){
-//                $('#output').append(data[i]["city_feature"] + "<br>");
-                var typeMap = [];
-
-//                var evtclrMap = {};
-                for (var i = 0; i < data.length; i++) {
-                    //string of events saved here
-                    var dat = data[i];
-                    typeMap.push(data[i]);
-//                    var dat = data[i]["city_feature"];
-//                    
-//                    counting the amount of times a string occurs
-//                    if (dat !== null) {
-//                        if (typeMap[dat]) {
-//                            typeMap[dat]++;
-//                        } else {
-//                            typeMap[dat] = 1;
-//                        }
-//                    }
-                }
-                //print contents of hashmap string and count recorded
-                $('#output').append("<div>INITIAL TYPE GROUP</div><br>");
-//                for (var type in typeMap) {
-//                    var p = type;
-//                    if (type !== 'undefined') {
-////                        $('#output').append("<div>" + type + " : " + typeMap[type] + "</div><br>");
-//                        $('#output').append("<div>" + type + "</div><br>");
-//                    }
-//
-//                }
-                for(var i = 0; i < typeMap.length; i++){
-                    if(typeMap[i] !== 'undefined'){
-                        $('#output').append("<div>" + typeMap[i].address + "</div><br>");
-                    }
-                }
-//            });
+                countCityFeatures(data);
+                getCultureData(data);
             });
+        }
+    }
+    function countCityFeatures(data) {
+        $('#output').empty().append("<div id='output'></div><br>");
+        var typeMap = {};
+//                var evtclrMap = {};
+        for (var i = 0; i < data.length; i++) {
+            //string of events saved here
+            var dat = data[i]["city_feature"];
+            //counting the number of times a String appears
+            if (dat !== null) {
+                if (typeMap[dat]) {
+                    typeMap[dat]++;
+                } else {
+                    typeMap[dat] = 1;
+                }
+            }
+        }
+        //print contents of hashmap string and count recorded
+        $('#output').append("<h3>CITY FEATURE COUNT</h3><br>");
+        for (var type in typeMap) {
+            if (type !== 'undefined') {
+                $('#output').append("<div>" + type + " : " + typeMap[type] + "</div><br>");
+//                        $('#output').append("<div>" + type + "</div><br>");
+            }
+        }
+    }
+    function getCultureData(data) {
+//        $('#output').empty().append("<div id='output'></div><br>");
+        var typeMap = [];
+        $('#output').append("<h3>ADDRESSES</h3><br>");
+        for(var i = 0; i < data.length; i++){
+            typeMap.push(data[i]);
+        }
+        for (var i = 0; i < typeMap.length; i++) {
+            if (typeMap[i] !== 'undefined') {
+                $('#output').append("<div>" + typeMap[i].address + "</div><br>");
+            }
         }
     }
 });
