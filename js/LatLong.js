6d2b586415f5291f4a5f3940342efca45b1606c7
SamuelioX <samueliox@gmail.com>
2017-01-17 02:33:57 -0800

    added geocoding

--- js/LatLong.js
+++ js/LatLong.js
@@ -7,21 +7,24 @@
 /* global type */
 
 $(function () {
+
     $('#json').click(function (e) {
         e.preventDefault();
         json();
     });
 
-    function json() {
+    $('#geocode').click(function (e) {
+        e.preventDefault();
+        geocodeAddress();
+    });
 
+    function json() {
         var latit = (document.getElementById('lat').value);
         var longit = (document.getElementById('lng').value);
-        var limit = 0;
-//        var limit = document.getElementById('limit').value;
         var radius = document.getElementById('rad').value;
 //        radius = parseInt(document.getElementById('rad').value) * 1609;
         if (latit === null || latit === "" || longit === null || longit === "" ||
-                radius === null || radius === "" || limit === null || limit === "") {
+                radius === null || radius === "") {
             alert("The form must be filled");
             return false;
         } else {
@@ -29,10 +32,10 @@ $(function () {
                 url: "https://data.seattle.gov/resource/3c4b-gdxv.json?$$app_token=IZLnwcjjGNvFpmxfooid8p5VI",
                 type: "GET",
                 data: {
-                    "$limit": 10000
+                    "$limit": 10000,
 ////                "$where": "longitude=-122.282103194"
-//                    "$where": "within_circle(location, " + latit + ", " + longit + ", " +
-//                            radius + ")"
+                    "$where": "within_circle(location, " + latit + ", " + longit + ", " +
+                            radius + ")"
                 }
             }).done(function (data) {
                 countCityFeatures(data);
@@ -41,7 +44,7 @@ $(function () {
         }
     }
     function countCityFeatures(data) {
-        $('#output').empty().append("<div id='output'></div><br>");
+        $('#output').empty();
         var typeMap = {};
 //                var evtclrMap = {};
         for (var i = 0; i < data.length; i++) {
@@ -66,10 +69,9 @@ $(function () {
         }
     }
     function getCultureData(data) {
-//        $('#output').empty().append("<div id='output'></div><br>");
         var typeMap = [];
         $('#output').append("<h3>ADDRESSES</h3><br>");
-        for(var i = 0; i < data.length; i++){
+        for (var i = 0; i < data.length; i++) {
             typeMap.push(data[i]);
         }
         for (var i = 0; i < typeMap.length; i++) {
@@ -77,5 +79,33 @@ $(function () {
                 $('#output').append("<div>" + typeMap[i].address + "</div><br>");
             }
         }
+    }
+
+    function geocodeAddress() {
+        var address = document.getElementById('address').value;
+        $.ajax({
+            url: "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyAGjI71ShfO3kMt8NouBuHFE_8IJP_CJ3w",
+            type: "GET",
+            data: {
+                "address": address
+            },
+            success: (function (data) {
+//            $('#output').append("<div>" + data.results[0].geometry.location.lat  + " " + data.results[0].geometry.location.lng + "</div><br>");
+//            console.log(data.results[0].geometry.location );
+                $('#output').empty();
+                if (data.status === "ZERO_RESULTS") {
+                    $('#output').append("<div>" + "Services are down." + "</div><br>");
+                    $('#lat')[0].value = "";
+                    $('#lng')[0].value = "";
+                } else {
+                    $('#lat')[0].value = data.results[0].geometry.location.lat;
+                    $('#lng')[0].value = data.results[0].geometry.location.lng;
+                }
+            }),
+            error: (function () {
+                $('#output').empty();
+                $('#output').append("<div>" + "Services are down." + "</div><br>");
+            })
+        });
     }
 });
