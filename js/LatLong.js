897b12a5037652126e80bf4bb6525f997eab4749
SamuelioX <samueliox@gmail.com>
2017-01-24 21:23:10 -0800

    no message

--- js/LatLong.js
+++ js/LatLong.js
@@ -33,20 +33,21 @@ $(function () {
                 type: "GET",
                 data: {
                     "$limit": 10000,
-////                "$where": "longitude=-122.282103194"
                     "$where": "within_circle(location, " + latit + ", " + longit + ", " +
-                            radius + ")"
+                            radius + ")AND(city_feature = 'Heritage Trees' " +
+                            "OR city_feature = 'Viewpoints' OR city_feature = 'Museums and Galleries' " +
+                            "OR city_feature = 'General Attractions' OR city_feature = 'Waterfront' " +
+                            "OR city_feature = 'Public Art' OR city_feature = 'Libraries')"
                 }
             }).done(function (data) {
                 countCityFeatures(data);
-                getCultureData(data);
             });
         }
     }
     function countCityFeatures(data) {
         $('#output').empty();
         var typeMap = {};
-//                var evtclrMap = {};
+        var dataMap = [];
         for (var i = 0; i < data.length; i++) {
             //string of events saved here
             var dat = data[i]["city_feature"];
@@ -54,31 +55,52 @@ $(function () {
             if (dat !== null) {
                 if (typeMap[dat]) {
                     typeMap[dat]++;
+                    dataMap.push(data[i]);
                 } else {
                     typeMap[dat] = 1;
+                    dataMap.push(data[i]);
                 }
             }
         }
         //print contents of hashmap string and count recorded
-        $('#output').append("<h3>CITY FEATURE COUNT</h3><br>");
+        $('#output').append("<h3>CITY FEATURE COUNT</h3>");
+        printCityFeatureCount(typeMap);
+        getCultureData(dataMap);
+    }
+
+    function checkFeature(dat) {
+        return (dat === "Libraries" || dat === "Heritage Trees" || dat === "Viewpoints" || dat === "Museums and Galleries" ||
+                dat === "General Attractions" || dat === "Ceremonies" || dat === "Waterfront" ||
+                dat === "Public Art");
+    }
+    
+    function printCityFeatureCount(typeMap){
         for (var type in typeMap) {
             if (type !== 'undefined') {
-                $('#output').append("<div>" + type + " : " + typeMap[type] + "</div><br>");
-//                        $('#output').append("<div>" + type + "</div><br>");
+                $('#output').append(type + " : " + typeMap[type] + "<br>");
             }
         }
     }
-    function getCultureData(data) {
-        var typeMap = [];
-        $('#output').append("<h3>ADDRESSES</h3><br>");
-        for (var i = 0; i < data.length; i++) {
-            typeMap.push(data[i]);
-        }
+    
+    function getCultureData(typeMap) {
+        $('#output').append("<h3>CITY FEATURES</h3>");
+        var content = "<table><tr><th>Name</th><th>Address</th><th>City Feature</th><th>Website</th></tr>";
         for (var i = 0; i < typeMap.length; i++) {
             if (typeMap[i] !== 'undefined') {
-                $('#output').append("<div>" + typeMap[i].address + "</div><br>");
+                var name = typeMap[i].common_name == null ? "" : typeMap[i].common_name;
+                var address = typeMap[i].address == null ? "" : typeMap[i].address;
+                var city_feature = typeMap[i].city_feature == null ? "" : typeMap[i].city_feature;
+                var website = typeMap[i].website == null ? "" : typeMap[i].website;
+
+                content += '<tr><td>' + name + '</td><td>' +
+                        address + '</td><td>' +
+                        city_feature + '</td><td>' +
+                        website + '</td></tr>';
             }
         }
+        content += "</table>";
+
+        $('#output').append(content);
     }
 
     function geocodeAddress() {
@@ -90,8 +112,6 @@ $(function () {
                 "address": address
             },
             success: (function (data) {
-//            $('#output').append("<div>" + data.results[0].geometry.location.lat  + " " + data.results[0].geometry.location.lng + "</div><br>");
-//            console.log(data.results[0].geometry.location );
                 $('#output').empty();
                 if (data.status === "ZERO_RESULTS") {
                     $('#output').append("<div>" + "Services are down." + "</div><br>");
